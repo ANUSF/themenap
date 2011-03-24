33dd3c1e05dddbd66feb9cfa7a5a29fe47eab056
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-03-24 16:26:41 +1100

    Added some configuration options for the Themenap::Nap constructor.

--- lib/themenap/nap.rb
+++ lib/themenap/nap.rb
@@ -2,23 +2,28 @@ require 'nokogiri'
 
 module Themenap
   class Nap
-    def initialize(server_uri)
+    def initialize(server_uri, options = {})
+      path = options[:path] || File.join('tmp', 'layouts')
+      name = options[:name] || 'theme.html.erb'
+      include_cmd = options[:include] || 'yield'
+
       response = Net::HTTP.get URI.parse(server_uri)
 
       doc = Nokogiri::HTML(response)
-      doc.css('article').each { |article| article.content = '{{= yield }}' }
+      doc.css('article').each do |article|
+        article.content = "{{= #{include_cmd} }}"
+      end
 
       ['src', 'href'].each do |attr|
         doc.css("*[#{attr}]").each do |node|
           link = node[attr]
-          node[attr] = "#{server_uri}#{link}" if link.starts_with? '/'
+          node[attr] = "#{server_uri}#{link}" if link.start_with? '/'
         end
       end
 
-      path = File.join(Rails.root, 'tmp', 'layouts')
       FileUtils.mkpath(path)
-      open(File.join(path, 'theme.html.erb'), 'w') do |fp|
-        fp.write doc.to_html.gsub(/{{/, '<%').gsub(/}}/, '%>')
+      open(File.join(path, name), 'w') do |fp|
+        fp.write doc.to_html.gsub(/\{\{/, '<%').gsub(/\}\}/, '%>')
       end
     end
   end
