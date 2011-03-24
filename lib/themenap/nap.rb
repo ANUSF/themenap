b2137a23757e2fb9a94f5bec2c61895876d28657
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-03-24 17:01:21 +1100

    The document title can be customised.

--- lib/themenap/nap.rb
+++ lib/themenap/nap.rb
@@ -3,17 +3,17 @@ require 'nokogiri'
 module Themenap
   class Nap
     def initialize(server_uri, options = {})
-      path = options[:path] || File.join('tmp', 'layouts')
-      name = options[:name] || 'theme.html.erb'
-      include_cmd = options[:include] || 'yield'
+      # -- process the option value passed
+      path   = options[:path] || File.join('tmp', 'layouts')
+      name   = options[:name] || 'theme.html.erb'
+      y_ield = options[:yield] || 'yield'
+      main   = options[:main] || ''
 
+      # -- grab the HTML page from the server and pass it
       response = Net::HTTP.get URI.parse(server_uri)
-
       doc = Nokogiri::HTML(response)
-      doc.css('article').each do |article|
-        article.content = "{{= #{include_cmd} }}"
-      end
 
+      # -- globalize links contained in the document
       ['src', 'href'].each do |attr|
         doc.css("*[#{attr}]").each do |node|
           link = node[attr]
@@ -21,6 +21,16 @@ module Themenap
         end
       end
 
+      # -- turn into a template
+      doc.css('article').each do |article|
+        article.content = "{{= #{y_ield} #{main} }}"
+      end
+
+      doc.css('title').each do |title|
+        title.content = "{{= #{y_ield} :title }}"
+      end
+
+      # -- write the result to a file
       FileUtils.mkpath(path)
       open(File.join(path, name), 'w') do |fp|
         fp.write doc.to_html.gsub(/\{\{/, '<%').gsub(/\}\}/, '%>')
